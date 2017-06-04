922c26b320b2fa36d29e08b842b680cf651822c4
Gelin Luo <greenlaw110@gmail.com>
2017-06-04 22:46:45 +1000

    fix issue with injecting EbeanServer into EbeanDao

--- src/main/java/act/db/ebean2/EbeanDaoInjectionListener.java
+++ src/main/java/act/db/ebean2/EbeanDaoInjectionListener.java
@@ -5,14 +5,12 @@ import act.app.App;
 import act.app.event.AppEventId;
 import act.db.DbService;
 import act.db.di.DaoInjectionListenerBase;
-import act.event.AppEventListenerBase;
 import org.osgl.$;
 import org.osgl.inject.BeanSpec;
 import org.osgl.util.Generics;
 import org.osgl.util.S;
 
 import java.lang.reflect.Type;
-import java.util.EventObject;
 import java.util.List;
 
 public class EbeanDaoInjectionListener extends DaoInjectionListenerBase {
@@ -37,13 +35,13 @@ public class EbeanDaoInjectionListener extends DaoInjectionListenerBase {
         if (dbService instanceof EbeanService) {
             final EbeanService service = $.cast(dbService);
             final EbeanDao dao = $.cast(injectee);
-            Act.eventBus().bind(AppEventId.DB_SVC_LOADED, new AppEventListenerBase(S.concat(resolved._2, "-ebean-on-dao-injection")) {
+            Act.jobManager().on(AppEventId.DB_SVC_LOADED, S.concat(resolved._2, "-ebean-on-dao-injection"), new Runnable() {
                 @Override
-                public void on(EventObject eventObject) throws Exception {
+                public void run() {
                     dao.ebean(service.ebean());
                     dao.modelType(resolved._1);
                 }
-            });
+            }, true);
         }
     }
 }
